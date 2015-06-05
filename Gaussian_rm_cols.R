2b87f1023a1e30b41296c33972612c9463ff954d
buckeye76guy <hounyo.1@osu.edu>
2015-06-04 20:15:05 -0400

    Notice Something
    Now I just Noticed That my pivot does not take the minimum absolute
    value! I will change that

--- Gaussian_rm_cols.R
+++ Gaussian_rm_cols.R
@@ -133,3 +133,75 @@ Gaussian_rref <- function(A, simplify = FALSE){
     return(B)
   }
 }
+
+## Compute Determinant of a matrix
+
+# I will use the row opertations to get the determinant
+# For this I will readjust the Gaussian_rref function
+
+Gaussian_rref_adj <- function(A){
+  # We mus not simplify
+  # Adjust so that we keep track of any swap of rows
+  # Adjust so that we only subtract or add the pivot row
+  # Remove unecessary lines
+  # At this I just realized that this does not guarantee
+  # That the matrix comes out as Upper triangular
+  m_rows <- nrow(A) # Matrix nber of rows
+  m_cols <- ncol(A) # Matrix nber of columns
+  B <- A # Just assign A to B. This keeps me from changing all
+  # instances of B in the code
+  # Now Let's Focus On The Elimination
+  count <- 0 # This will determine how many time we swap rows
+  for(i in 1:(m_rows - 1)){
+    # Get lowest coefficient for cuurent variable
+    ind <- which(B[,i] == min(B[B[,i] != 0, i]))[1]
+    # This determines if we have to swap rows
+    if(ind != i){
+      # I just realized that this if statement can save time
+      # I will do the same to the previous Gaussian_rref
+      count <- count + 1
+      temp <- B[i,] # Save first row as temporary
+      B[i,] <- B[ind,] # Set first row to have lowest coef
+      B[ind,] <- temp # Complete the swap of rows
+    }
+    
+    pivot <- B[i,i] # Get leading coef of pivot row
+    
+    for(j in (i+1):m_rows){
+      mul = B[j,i] # Get leading coef of current row
+      # Update current row to eliminate leading coef
+      B[j,] <- B[j,]  - (mul/pivot)*B[i,]
+    }
+  } 
+  
+  return(B)
+}
+
+Rref_det <- function(A){
+  if(!is.matrix(A)){
+    stop("Matrices Only!")
+  }
+  m_rows <- nrow(A) # Matrix rows
+  m_cols <- ncol(A) # Matrix columns
+  # No Determinant for non-square matrices
+  if(m_rows != m_cols){
+    stop("Cannot Perform This Operation On A Non-Sqaure Matrix")
+  }
+  # If There are any zero rows or columns then return 0
+  # m_rows = m_cols so we can check both at once
+  for(i in 1:m_rows){
+    if(all(A[i,] == 0) || all(A[,i] == 0)) {
+      return(0)
+    }
+  }
+  # If There are any 2 linearly dependent rows or columns => 0
+  for(i in 1:(m_rows - 1)){
+    for(j in (i + 1):m_rows){
+      if(Linear_check(A[i,], A[j,]) || 
+           Linear_check(A[,i], A[,j])){
+        return(0)    
+      }
+    }
+  }
+  # Now we can perform the adjusted rref function
+}
